6a11ef5d6e6a69f8f6e04f56b73adfc3b8ebbdee
WnndGws <WnndGws@users.noreply.github.com>
2020-04-06 03:10:01 +0800

    No need for loop since handled by cron now

--- dotfiles/laptop/.config/waybar/battery_brightness.sh
+++ dotfiles/laptop/.config/waybar/battery_brightness.sh
@@ -4,26 +4,21 @@
 light -N 10
 # Sets minimum brightness to 10%
 
-while true;
-do
-    battery_percentage=$(acpi --battery | rg -o '[0-9]+%' | awk -F '%' '{print $1}')
-    set_percentage=$(bc <<< $battery_percentage/1.5)
-    set_percentage=$(printf "%.0f" "$set_percentage/5")
-    set_percentage=$(bc <<< $set_percentage*5)
-    # Rounds set_percentage to nearest 5
-    echo $set_percentage
+battery_percentage=$(acpi --battery | rg -o '[0-9]+%' | awk -F '%' '{print $1}')
+set_percentage=$(bc <<< $battery_percentage/1.5)
+set_percentage=$(printf "%.0f" "$set_percentage/5")
+set_percentage=$(bc <<< $set_percentage*5)
+# Rounds set_percentage to nearest 5
+echo $set_percentage
 
-    battery_state=$(acpi --battery | awk '{print $3}')
+battery_state=$(acpi --battery | awk '{print $3}')
 
-    if [[ $battery_state == "Charging," ]]; then
+if [[ $battery_state == "Charging," ]]; then
+    light -S 100
+else
+    if [[  $battery_state == "Full," ]]; then
         light -S 100
     else
-        if [[  $battery_state == "Full," ]]; then
-            light -S 100
-        else
-            light -S $set_percentage
-        fi
+        light -S $set_percentage
     fi
-
-    sleep 300
-done
+fi
