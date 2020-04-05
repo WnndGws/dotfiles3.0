61f6086a97fb43e01e12315b4ab42954df39d617
WnndGws <WnndGws@users.noreply.github.com>
2020-04-06 00:36:42 +0800

    Loop

--- dotfiles/laptop/.config/waybar/battery_brightness.sh
+++ dotfiles/laptop/.config/waybar/battery_brightness.sh
@@ -4,21 +4,26 @@
 light -N 10
 # Sets minimum brightness to 10%
 
-battery_percentage=$(acpi --battery | rg -o '[0-9]+%' | awk -F '%' '{print $1}')
-set_percentage=$(bc <<< $battery_percentage/1.5)
-set_percentage=$(printf "%.0f" "$set_percentage/5")
-set_percentage=$(bc <<< $set_percentage*5)
-# Rounds set_percentage to nearest 5
-echo $set_percentage
+while true;
+do
+    battery_percentage=$(acpi --battery | rg -o '[0-9]+%' | awk -F '%' '{print $1}')
+    set_percentage=$(bc <<< $battery_percentage/1.5)
+    set_percentage=$(printf "%.0f" "$set_percentage/5")
+    set_percentage=$(bc <<< $set_percentage*5)
+    # Rounds set_percentage to nearest 5
+    echo $set_percentage
 
-battery_state=$(acpi --battery | awk '{print $3}')
+    battery_state=$(acpi --battery | awk '{print $3}')
 
-if [[ $battery_state == "Charging," ]]; then
-    light -S 100
-else
-    if [[  $battery_state == "Full," ]]; then
+    if [[ $battery_state == "Charging," ]]; then
         light -S 100
     else
-        light -S $set_percentage
+        if [[  $battery_state == "Full," ]]; then
+            light -S 100
+        else
+            light -S $set_percentage
+        fi
     fi
-fi
+
+    sleep 300
+done
