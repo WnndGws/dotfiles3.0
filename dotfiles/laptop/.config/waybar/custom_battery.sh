720b8c92352ed97fb6eab156edf9ccce1590e802
WnndGws <WnndGws@users.noreply.github.com>
2020-04-11 05:10:17 +0800

    Refactoring to improve efficiency

--- dotfiles/laptop/.config/waybar/custom_battery.sh
+++ dotfiles/laptop/.config/waybar/custom_battery.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-STATE=`acpi | awk -F '0: ' '{print $2}' | cut -d',' -f1`
-PERCENT=`acpi | awk -F ', ' '{print $2}' | cut -d'%' -f1`
-TIME=`acpi | awk -F ', ' '{print $3}' | cut -d':' -f1-2`
-if [ -z "$TIME" ]; then
-    TIME=$STATE
-fi
+STATE=$(cat /sys/class/power_supply/BAT0/status)
+PERCENT=$(cat /sys/class/power_supply/BAT0/capacity)
+TIME=$(acpi | awk -F ', ' '{print $3}' | cut -d':' -f1-2)
+[ -z "$TIME" ] && TIME=$STATE
 
-if [ $PERCENT -le 30 ] && [ $STATE != 'Charging' ]; then
-    STATE='Low'
-fi
+[ $PERCENT -le 30 ] && [ $STATE != 'Charging' ] && STATE='Low'
 
 echo -e "{\"text\":\"$TIME\", \"percentage\":$PERCENT, \"class\":\"$STATE\"}"
